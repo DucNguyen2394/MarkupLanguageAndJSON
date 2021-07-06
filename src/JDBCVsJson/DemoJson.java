8d8707f5374bc9ebc8655dae4f958710dfdbd743
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-07-06 08:30:43 +0700

    update code

---
+++ src/JDBCVsJson/DemoJson.java
@@ -0,0 +1,65 @@
+package JDBCVsJson;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class DemoJson {
+    public static Connection getConnection() throws Exception{
+        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        //Getting the connection
+        String mysqlUrl = "jdbc:mysql://localhost/studentdemo";
+        Connection con = DriverManager.getConnection(mysqlUrl, "root", "ducnguyen@94");
+        System.out.println("Connection established......");
+        return con;
+    }
+
+    public static void main(String[] args) {
+        JSONParser jsonParser = new JSONParser();
+        try {
+            //Parsing the contents of the JSON file
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("src/JDBCVsJson/players.json"));
+            //Retrieving the array
+            JSONArray jsonArray = (JSONArray) jsonObject.get("players_data");
+            Connection con = getConnection();
+            //Insert a row into the MyPlayers table
+            PreparedStatement pstmt = con.prepareStatement("INSERT INTO players values (?,?, ?, ?, ?, ? )");
+            System.out.println("a " + jsonArray);
+            for(Object object : jsonArray) {
+                JSONObject record = (JSONObject) object;
+                int id = Integer.parseInt((String) record.get("ID"));
+                String first_name = (String) record.get("First_Name");
+                String last_name = (String) record.get("Last_Name");
+                String date = (String) record.get("Date_Of_Birth");
+                long date_of_birth = Date.valueOf(date).getTime();
+                String place_of_birth = (String) record.get("Place_Of_Birth");
+                String country = (String) record.get("Country");
+                pstmt.setInt(1, id);
+                pstmt.setString(2, first_name);
+                pstmt.setString(3, last_name);
+                pstmt.setDate(4, new Date(date_of_birth));
+                pstmt.setString(5, place_of_birth);
+                pstmt.setString(6, country);
+                pstmt.executeUpdate();
+            }
+            System.out.println("Records inserted.....");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
